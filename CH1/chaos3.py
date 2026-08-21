de1f0745ed3187716450ea0810ae977eb3906383
dphicode <dphilli9@smartweb.augustatech.edu>
2026-08-20 22:33:12 -0400

    Upload CH1 assignment
    Chaos1-5.py + HW txt

---
+++ CH1/chaos3.py
@@ -0,0 +1,11 @@
+# File: chaos.py
+# A simple proram illustrating chaotic behavior.
+
+def main():
+    print("This program illustrates a chaotic function")
+    x = float(input("Enter a number between 0 and 1: "))
+    k = 3.44905
+    for i in range(30):
+        x = k * x * (1 - x)
+        print(x)
+main()
---
+++ CH1/chaos5.py
@@ -0,0 +1,11 @@
+# File: chaos.py
+# A simple proram illustrating chaotic behavior.
+
+def main():
+    print("This program illustrates a chaotic function")
+    x = float(input("Enter a number between 0 and 1: "))
+    k = float(input("Enter a value for var k"))
+    for i in range(30):
+        x = k * x * (1 - x)
+        print(x)
+main()
