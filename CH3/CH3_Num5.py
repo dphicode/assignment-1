583d37765a7f24a1cc16f6f0d1e7a1aac78d8923
dphicode <dphilli9@smartweb.augustatech.edu>
2026-09-13 15:09:39 -0400

    Re-upload of CH3 1-5.
    Upload assignments CH3 1-5.

---
+++ CH3/CH3_Num5.py
@@ -0,0 +1,19 @@
+import math
+def calc(payment,intr):
+    term = 15
+    for i in range(2):
+        cost = payment * ((1 - (1 + intr / 12) ** (-term * 12)) / (intr / 12))
+        print(f"{term} loan term will afford a house of {cost}")
+        term = 30
+
+def inputs():
+    payment = float(input("What is your monthly payment? "))
+    interest = float(input("What is the interest rate? (compounded monthly) "))
+    interest = interest / 100
+    calc(payment,interest)
+
+print("Let's calculate how much you can afford to finance.")
+run = input("Ready to calculate? (y/n) ")
+while run == "y":
+    inputs()
+    run = input("Ready to calculate? (y/n) ")
