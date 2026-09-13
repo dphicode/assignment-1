1dba299ffdcb83a471e8f62eee0228689584c272
dphicode <dphilli9@smartweb.augustatech.edu>
2026-09-13 15:06:19 -0400

    Update CH3_Num5.py

--- CH3/CH3_Num5.py
+++ CH3/CH3_Num5.py
@@ -1,6 +1,5 @@
 import math
 def calc(payment,intr):
-    #payment = cost / ((1 - (1 + intr / 12) ** (-term * 12)) / (intr / 12))
     term = 15
     for i in range(2):
         cost = payment * ((1 - (1 + intr / 12) ** (-term * 12)) / (intr / 12))
